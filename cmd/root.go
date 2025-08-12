926d82f91eee3ceae05416d36d82ebcf2b896704
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2025-08-12 10:24:57 +0500

    batman: first commit

---
+++ cmd/root.go
@@ -0,0 +1,46 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "Tnotes",
+	Short: "A fast terminal notes and knowledge keeping tool",
+	Long: `Tnotes stand for Terminal notes is a full fledged CLI utility
+whose main purpose is to divide the notes and TODO in a meaning full 
+way for users to get Knowledge from it.
+Tnotes allow you to add, edit, delete, list, and search all the notes.
+One another thing is to divide the notes through settings.`,
+	Version: "0.1",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.UmairAhmedImran.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
