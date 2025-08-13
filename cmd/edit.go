520e835a3c7911bb5b60a1fe686509d31c7864d5
umairahmedimran <umairahmedimranbutt@gmail.com>
2025-08-13 19:20:27 +0500

    feat: created basic commands

---
+++ cmd/edit.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// editCmd represents the edit command
+var editCmd = &cobra.Command{
+	Use:   "edit",
+	Short: "edit the notes",
+	Long: `edit the notes based on the name that has been specified
+if no notes of that is found then error is shown`,
+  Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("edit called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(editCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// editCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// editCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
