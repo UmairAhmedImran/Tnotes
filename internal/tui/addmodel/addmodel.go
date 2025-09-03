854712ec5b3039cdf2a674b9b3af4eb5b18ad2e3
umairahmedimran <umairahmedimranbutt@gmail.com>
2025-09-03 18:49:55 +0500

    chore(feat): created addmodel.go for add model

--- internal/tui/addmodel/addmodel.go
+++ internal/tui/addmodel/addmodel.go
@@ -1,8 +1,40 @@
 package addmodel
 
 import (
-  "fmt"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+type Model struct {
+	input string
+	done  bool
+}
 
+func New() Model {
+	return Model{}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "enter":
+			m.done = true
+			// You might send a message back to root model to save
+			return m, tea.Quit
+		case "esc":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m Model) View() string {
+	if m.done {
+		return "Note saved! Press any key to return.\n"
+	}
+	return "Add a note:\n(Type your note and press Enter)\n"
+}
